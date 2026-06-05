a23cd83b8fc2624cf0b845c45537bfb344cc7ce5
gizer07873217-stack <gizer07873217@gmail.com>
2026-06-05 12:31:42 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'iidx-tracker-v1';
+const FILES = [
+  './index2.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.open(CACHE_NAME).then(cache =>
+      fetch(e.request).then(res => {
+        cache.put(e.request, res.clone());
+        return res;
+      }).catch(() => caches.match(e.request))
+    )
+  );
+});
